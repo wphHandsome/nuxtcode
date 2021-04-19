e498cf2cb4d6a7ef354646b8cb4d66637713fa30
王鹏贺 <wangpenghe@WphMacBook-Pro.local>
2021-04-19 14:40:42 +0800

    部署

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- 顶部导航栏 -->
+    <!-- 顶部导航栏 111-->
     <nav class="navbar navbar-light">
       <div class="container">
         <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
