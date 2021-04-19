366afa8e3d7f3a2cb8bf55f2dc2e6fd6fdc70923
王鹏贺 <wangpenghe@WphMacBook-Pro.local>
2021-04-19 14:33:57 +0800

    nuxt部署

---
+++ pages/settings/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="settings-page">
+    <div class="container page">
+      <div class="row">
+
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Your Settings</h1>
+
+          <form>
+            <fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control" type="text" placeholder="URL of profile picture">
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+                </fieldset>
+                <fieldset class="form-group">
+                  <textarea class="form-control form-control-lg" rows="8" placeholder="Short bio about you"></textarea>
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="text" placeholder="Email">
+                </fieldset>
+                <fieldset class="form-group">
+                  <input class="form-control form-control-lg" type="password" placeholder="Password">
+                </fieldset>
+                <button class="btn btn-lg btn-primary pull-xs-right">
+                  Update Settings
+                </button>
+            </fieldset>
+            <button class="btn btn-outline-danger"  @click="onlogout">
+                   Or click here to logout.
+            </button>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  middleware: 'authenticated',
+  name: 'SettingsIndex',
+  methods:{
+    onlogout(){
+       this.$store.commit('setUser', '')
+        // 为了防止刷新页面数据丢失，我们需要把数据持久化
+        Cookie.set('user', '')
+        // 跳转到首页
+        this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
